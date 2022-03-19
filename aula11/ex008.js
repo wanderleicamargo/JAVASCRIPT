d1bb5972e3aeaa3ce4add7c05371183e939a2d03
wanderleicamargo <101301705+wanderleicamargo@users.noreply.github.com>
2022-03-19 12:41:29 -0300

    java

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel=81
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel>80){
+    console.log(`Você excedeu o limite de velocidade em ${vel-80}Km/h`)
+}
+    else{console.log('Você não excedeu os limites de velocidade')}
